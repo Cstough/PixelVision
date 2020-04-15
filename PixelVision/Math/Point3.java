3d6b01cc554a764edf326a4cf4ff739fbd49a621
Cstough <32025644+Cstough@users.noreply.github.com>
2020-04-15 17:54:06 -0400

    Fixed Directories

---
+++ PixelVision/Math/Point3.java
@@ -0,0 +1,13 @@
+package PixelVision.Math;
+
+public final class Point3 {
+
+    public float x, y, z;
+
+    public Point3(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
